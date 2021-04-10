769703b883b0528418a7864646ca6ade92fcdb84
Dung Truong <dt.young112@gmail.com>
2021-04-10 00:11:22 -0700

    first clean commit

---
+++ load_data_master.m
@@ -0,0 +1,162 @@
+function load_data_master(winLength, numChan, isSpectral, isTopo)
+% clear
+% Add EEGLAB path
+addpath('/expanse/projects/nemar/eeglab');
+eeglab; close;
+
+%try, parpool(23); end
+
+folderout = '/expanse/projects/nemar/child-mind-restingstate-preprocessed';
+fileNamesClosed = dir(fullfile(folderout, '*_eyesclosed.set'));
+female = readtable('female.csv');
+female = female.Var1;
+male = readtable('male.csv');
+male = male.Var1;
+N = length(female)*2;
+% choose training, validation, and test from different subjects.
+N_test_subjs = ceil(N * 0.125);
+N_val_subjs = ceil(N * 0.3125);
+N_train_subjs = length(fileNamesClosed) - N_test_subjs - N_val_subjs;
+
+subj_data = cell(1,N);
+subj_gender = zeros(1,N);
+subj_IDs = cell(1,N);
+
+% max_sample_per_subj = 85;
+% X_train = cell(1,N_train_subjs*2*max_sample_per_subj); % 2 is number of genders
+% Y_train = cell(1,N_train_subjs*2*max_sample_per_subj);
+% X_val = cell(1,N_val_subjs*2*max_sample_per_subj);
+% Y_val = cell(1,N_val_subjs*2*max_sample_per_subj);
+% X_test = cell(1,N_test_subjs*2*max_sample_per_subj);
+% Y_test = cell(1,N_test_subjs*2*max_sample_per_subj);
+% test_subjID = cell(1,N_test_subjs*2*max_sample_per_subj);
+% subjID = {};
+
+% dimension of number of sample in the data. If topo map, 4 (rgb x samples), otherwise 3
+% (chan x times x samples)
+if isTopo, sample_dim = 4; else, sample_dim = 3; end
+
+parfor iSubj=1:N
+    if mod(iSubj,2) == 1
+        % female
+        EEGeyesc = pop_loadset('filepath', folderout, 'filename', [female{iSubj} '_eyesclosed.set']);
+    else
+        % male
+        EEGeyesc = pop_loadset('filepath', folderout, 'filename', [male{iSubj/2} '_eyesclosed.set']);
+    end
+
+    % sub-sample using window length
+    EEGeyesc = eeg_regepochs( EEGeyesc2, 'recurrence', winLength, 'limits', [0 winLength]);
+    tmpdata = EEGeyesc.data;
+
+    % If numChan is 24, sub-select channel. Otherwise assuming it's 128
+    % which is the original data
+    if numChan == 24
+        % sub-select channel
+        channel_map = {'Fp1', 22; 'Fp2', 9; 'F7', 33;'F3',24;'Fz', 11;'F4',124;'F8', 122;'FC3', 29;'FCz', 6;'FC4', 111;'T3', 45;'C3', 36;
+            'C4', 104;'T4', 108;'CP3', 42;'CPz', 55;'CP4', 93;'T5', 58;'P3', 52;'Pz', 62;'P4', 92;'T6', 96;'O1', 70; 'Cz', 'Cz'};
+        chanindices = [];
+        for iChannel = 1:size(channel_map,1)
+            if ~ischar(channel_map{iChannel,2})
+                egiChannel = sprintf('E%d', channel_map{iChannel,2});
+                chanindices = [chanindices find(cellfun(@(x) strcmp(x,egiChannel), {EEGeyesc.chanlocs.labels}))];
+            else
+                chanindices = [chanindices find(cellfun(@(x) strcmp(x,'Cz'), {EEGeyesc.chanlocs.labels}))];
+            end
+        end
+        if (length(chanindices) < 24)
+        warning('%s have missing channels. Skipped', fileNamesClosed(iFile).name);
+            disp(size(tmpdata));
+        continue;
+        end 
+        tmpdata = tmpdata(chanindices,:,:);
+    end
+
+    % If compute spectral
+    if isSpectral
+        freqRanges = [4 7; 7 13; 14 30]; % frequencies, but also indices
+        % compute spectrum
+        srates = 100;
+        [XSpecTmp,~] = spectopo(tmpdata, winLength*1000, srates, 'plot', 'off', 'overlap', 50);
+        XSpecTmp(:,1) = []; % remove frequency 0
+
+        % get frequency bands
+        theta = mean(XSpecTmp(:, freqRanges(1,1):freqRanges(1,2)), 2);
+        alpha = mean(XSpecTmp(:, freqRanges(2,1):freqRanges(2,2)), 2);
+        beta  = mean(XSpecTmp(:, freqRanges(3,1):freqRanges(3,2)), 2);
+
+        %TODO: modify tmpdata to contains spectral and phase
+
+        if isTopo
+            % get grids
+            [~, gridTheta] = topoplot( theta, EEGeyesc.chanlocs, 'verbose', 'off', 'gridscale', 28, 'noplot', 'on', 'chaninfo', EEGeyesc(1).chaninfo);
+            [~, gridAlpha] = topoplot( alpha, EEGeyesc.chanlocs, 'verbose', 'off', 'gridscale', 28, 'noplot', 'on', 'chaninfo', EEGeyesc(1).chaninfo);
+            [~, gridBeta ] = topoplot( beta , EEGeyesc.chanlocs, 'verbose', 'off', 'gridscale', 28, 'noplot', 'on', 'chaninfo', EEGeyesc(1).chaninfo);
+            gridTheta = gridTheta(end:-1:1,:); % for proper imaging using figure; imagesc(grid);
+            gridAlpha = gridAlpha(end:-1:1,:); % for proper imaging using figure; imagesc(grid);
+            gridBeta  = gridBeta( end:-1:1,:); % for proper imaging using figure; imagesc(grid);
+
+            topoTmp = gridTheta;
+            topoTmp(:,:,3) = gridBeta;
+            topoTmp(:,:,2) = gridAlpha;
+            tmpdata = single(topoTmp);
+
+            % remove Nan
+            minval = nanmin(nanmin(tmpdata,[],1),[],2);
+            maxval = nanmax(nanmax(tmpdata,[],1),[],2);
+
+            % transform to RGB image
+            tmpdata = bsxfun(@rdivide, bsxfun(@minus, tmpdata, minval), maxval-minval)*255;
+            tmpdata(isnan(tmpdata(:))) = 0;
+        end
+    end
+
+    % append to XOri
+    subj_data{iSubj} = tmpdata;
+    subj_gender(iSubj) = EEGeyesc.gender;
+    subj_IDs{iSubj} = string(EEGeyesc.subjID);
+%     for sample=1:size(tmpdata,sample_dim)
+%         idx = iFile*2*max_sample_per_subj+gender*max_sample_per_subj+sample;
+%         if (iFile <= N_test_subjs)
+%             X_test{idx} = tmpdata(:,:,sample);
+%             Y_test{idx} = EEGeyesc.gender;
+%             test_subjID{idx} = string(EEGeyesc.subjID);
+%         elseif (iFile <= N_test_subjs + N_val_subjs)
+%             X_val{idx} = tmpdata(:,:,sample);
+%             Y_val{idx} = EEGeyesc.gender;
+%         else
+%             X_train{idx} = tmpdata(:,:,sample);
+%             Y_train{idx} = EEGeyesc.gender;
+%         end
+%     end
+end
+
+
+X_test = cat(sample_dim,subj_data{1:N_test_subjs});
+Y_test = subj_gender(1:N_test_subjs);
+test_subjID = subj_IDs(1, N_test_subjs);
+X_val = cat(sample_dim,subj_data{N_test_subjs+1:N_test_subjs + N_val_subjs});
+Y_val = subj_gender(N_test_subjs+1:N_test_subjs + N_val_subjs);
+X_train = cat(sample_dim,subj_data{N_test_subjs + N_val_subjs + 1:end});
+Y_train = subj_gender(N_test_subjs + N_val_subjs+1:end);
+
+% save
+param_text = ['_' num2str(winLength) 's'];
+param_text = [param_text '_' num2str(numChan) 'chan'];
+if isSpectral
+    if isTopo
+        param_text = [param_text '_topo'];
+    else
+        param_text = [param_text '_spectral'];
+    end
+else
+    param_text = [param_text '_raw'];
+end
+save(['child_mind_x_train' param_text '.mat'],'X_train','-v7.3');
+save(['child_mind_y_train' param_text '.mat'],'Y_train','-v7.3');
+save(['child_mind_x_val' param_text '.mat'],'X_val','-v7.3');
+save(['child_mind_y_val' param_text '.mat]'],'Y_val','-v7.3');
+save(['child_mind_x_test' param_text '.mat'],'X_test','-v7.3');
+save(['child_mind_y_test' param_text '.mat'],'Y_test','-v7.3');
+save('test_subj.mat','test_subjID','-v7.3');
+
---
+++ restingstate_prepare_clean_master.m
@@ -0,0 +1,99 @@
+clear
+% Add EEGLAB path
+addpath('../eeglab');
+eeglab; close;
+
+try, parpool; end
+
+% Path to data
+folderin  = '../child-mind-uncompressed';
+folderout = '../child-mind-restingstate-preprocessed';
+folders = dir(folderin);
+fprintf('length %f', length(folders));
+
+% Prepare data
+fileTrain = dir(fullfile(folderout, '*.set'));
+% read the CSV file
+disp('Loading info file...');
+info = loadtxt('HBN_all_Pheno.csv', 'delim', ',', 'verbose', 'off');
+info = info(2:end,:);
+
+XTrain = cell(1,length(folders));
+YTrain = cell(1,length(folders));
+issueFlag = cell(1, length(folders));
+count = 1;
+parfor iFold = 1:length(folders)
+    %for iFold = 1:6 %length(folders)
+    
+    fileName = fullfile(folders(iFold).folder, folders(iFold).name, 'EEG/raw/mat_format/RestingState.mat');
+    fileNameClosedSet = fullfile(folderout, [ folders(iFold).name '_eyesclosed.set' ]);
+    fileNameClosedFdt = fullfile(folderout, [ folders(iFold).name '_eyesclosed.fdt' ]);
+%     fileNameOpenSet   = fullfile(folderout, [ folders(iFold).name '_eyesopen.set' ]);
+%     fileNameOpenFdt   = fullfile(folderout, [ folders(iFold).name '_eyesopen.fdt' ]);
+    if exist(fileNameClosedSet, 'file')
+	disp([fileNameClosedSet ' exists. Skipped']);
+	continue;
+        %delete(fileNameClosedSet);
+        %delete(fileNameClosedFdt);
+    end
+    %if exist(fileNameOpenSet, 'file')
+        %delete(fileNameOpenSet);
+        %delete(fileNameOpenFdt);
+    %end
+    infoRow = strmatch(folders(iFold).name, info(:,1)', 'exact');
+    if exist(fileName, 'file') && length(infoRow) > 0
+        try
+            EEG = load(fileName);
+            EEG = EEG.EEG;
+            
+            if EEG.nbchan == 129
+                for iEvent2 = 1:length(EEG.event)
+                    EEG.event(iEvent2).latency = EEG.event(iEvent2).sample;
+                end
+                
+                % copy info
+                EEG.gender     = info{infoRow(1),2};
+                EEG.age        = info{infoRow(1),3};
+                EEG.handedness = info{infoRow(1),4};
+                EEG.subjID     = folders(iFold).name;
+                % get channel location and clean data
+                % chanlocs = pop_chanedit(struct('labels', {'cz' 'pz' 'fz' 'nz' 'tp9' 'tp10' 'lpa' 'rpa'})) 
+                % mastoids in eeg system is TP9 (maybe 68/69) TP10 
+                % then go on web: electrical geodesics 128 channel
+                % pop_chancoresp(chanlocs, EEG.chanlocs)
+                % plugin to do EOG-regression based 
+                EEG = pop_chanedit(EEG, 'load',{'GSN_HydroCel_129.sfp','filetype','autodetect'});
+                EEG = pop_select(EEG, 'nochannel', 129);
+                EEG = pop_rmbase(EEG, []);
+                EEG = pop_eegfiltnew(EEG, 'locutoff',0.25,'hicutoff',25);
+                EEG = pop_resample(EEG, 128);
+                % add Cz back and re-reference
+                EEG = pop_chanedit(EEG, 'append',131,'changefield',{132,'labels','Cz'},'changefield',{132,'theta','0'},'changefield',{132,'radius','0'},'changefield',{132,'X','5.2047e-15'},'changefield',{132,'Y','0'},'changefield',{132,'Z','85'},'changefield',{132,'sph_theta','0'},'changefield',{132,'sph_phi','90'},'changefield',{132,'sph_radius','85'},'setref',{'1:131','Cz'});
+                EEG = eeg_checkset( EEG );EEG = pop_reref( EEG, [],'refloc',struct('labels',{'Cz'},'Y',{0},'X',{5.2047e-15},'Z',{85},'sph_theta',{0},'sph_phi',{90},'sph_radius',{85},'theta',{0},'radius',{0},'type',{''},'ref',{''},'urchan',{[]},'datachan',{0}));
+                EEG = pop_reref( EEG, [57 100] );
+
+                
+%                 % resting state: instruction. Open 20 close 40 20 - 40
+%                 % wait 2.5 seconds. 
+%                 EEGeyeso = pop_epoch( NEWEEG, {  '20  ' }, [4  19], 'newname', 'Eyes open', 'epochinfo', 'yes');
+%                 EEGeyeso.eyesclosed = 0;
+%                 pop_saveset(EEGeyeso, fileNameOpenSet);
+                EEGeyesc = pop_epoch( EEG, {  '30  ' }, [3  37], 'newname', 'Eyes closed', 'epochinfo', 'yes');
+                EEGeyesc.eyesclosed = 1;
+                EEGeyesc = eeg_epoch2continuous(EEGeyesc);
+                EEGeyesc2 = pop_clean_rawdata(EEGeyesc, 'FlatlineCriterion',5,'ChannelCriterion',0.7,'LineNoiseCriterion',4,'Highpass','off','BurstCriterion','off','WindowCriterion','off','BurstRejection','off','Distance','Euclidian');
+                EEGeyesc2 = pop_interp(EEGeyesc2, EEGeyesc.chanlocs);
+                pop_saveset(EEGeyesc2, fileNameClosedSet);
+  
+            else
+                issueFlag{iFold} = 'Not 129 channels';
+            end
+        catch
+            issueFlag{iFold} = lasterr;
+        end
+    end
+end
+
+indIssue = find(~cellfun(@isempty, issueFlag));
+fprintf('Issues at indices (empty means no issues): %s\n', int2str(indIssue));
+if ~isempty(indIssue), issueFlag(indIssue)', end
